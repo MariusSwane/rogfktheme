9a22320f492ab358d705cf5ed5e0ba2e32ab5e48
mariusswane <marius.swane.wishman@rogfk.no>
2026-06-04 07:08:06 +0200

    reordered palette

--- R/rogfk_palette.r
+++ R/rogfk_palette.r
@@ -1,13 +1,13 @@
 rogfk_pal <- function(type = "discrete") {
   discrete <- c(
-    "#0069B4", "#FF4C2E", "#28AD78", "#FFA9FB", "#8874E3", "#25317A",
+    "#0069B4", "#28AD78", "#FFA9FB", "#8874E3", "#FF4C2E", "#25317A",
     "#5FC4E5", "#AFE2F2", "#1E4C45", "#252A35", "#D9FFA3"
   )
   continuous <- c(
     "#252A35", "#25317A", "#0069B4", "#5FC4E5", "#EFECE6"
   )
   fill <- c(
-    "#80B4DAFF", "#FFA597FF", "#93D6BCFF", "#FFD4FDFF", "#C3BAF1FF",
+    "#80B4DAFF", "#93D6BCFF", "#FFD4FDFF", "#C3BAF1FF", "#FFA597FF",
     "#9298BDFF", "#AFE2F2FF", "#ECFFD1FF", "#D7F1F9FF", "#8FA5A2FF"
   )
   if (type == "discrete") {
