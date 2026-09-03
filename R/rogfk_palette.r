a929b7261bcf11ae0148435443d28cd2bf594837
mariusswane <marius.swane.wishman@rogfk.no>
2026-09-03 12:14:04 +0200

    små justeringer på paletten. Rosa nedprioriteres

--- R/rogfk_palette.r
+++ R/rogfk_palette.r
@@ -1,7 +1,7 @@
 rogfk_pal <- function(type = "discrete") {
   discrete <- c(
-    "#0069B4", "#28AD78", "#FFA9FB", "#8874E3", "#FF4C2E", "#25317A",
-    "#5FC4E5", "#AFE2F2", "#1E4C45", "#252A35", "#D9FFA3"
+    "#0069B4", "#28AD78", "#D9FFA3", "#8874E3", "#FF4C2E", "#25317A",
+    "#5FC4E5", "#AFE2F2", "#1E4C45", "#FFA9FB", "#252A35"
   )
   continuous <- c(
     "#252A35", "#25317A", "#0069B4", "#5FC4E5", "#EFECE6"
